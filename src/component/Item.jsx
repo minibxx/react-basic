f047deff730ec29d7218533d64f31eec96fa1b4d
minibxx <alsdbqls1004@gmail.com>
2023-08-11 13:13:57 +0900

    투두리스트 목록 수정, 삭제, 카운트 완성

--- src/component/Item.jsx
+++ src/component/Item.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-function Item({item,todoStatus}) {
+function Item({item,todoStatus,deleteData,modifyData}) {
 
   return (
     <li className={item.state ? 'active':''}>
         <input type="checkbox" data-code={item.date} onChange={todoStatus}  />
-        {/* 온 체인지가 일어나면 todoStatus 함수 실행 */}
+        {/* 부모에게 todoS 값을 받아서 온 체인지가 일어나면 todoStatus 함수 실행 */}
         <code>{item.todo}</code>
-        <button disabled={item.state?true:false}>수정</button>
-        <button>삭제</button>
+        <button onClick={()=>modifyData(item.date)} disabled={item.state?true:false}>수정</button>
+        <button onClick={()=>{deleteData(item.date)}}>삭제</button>
     </li>
   )
 }

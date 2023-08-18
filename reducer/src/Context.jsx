5b17fb2a2eafc9c435f963a3ba1897778fc7cdab
minibxx <alsdbqls1004@gmail.com>
2023-08-18 10:27:57 +0900

    0818 reducer 수업

---
+++ reducer/src/Context.jsx
@@ -0,0 +1,21 @@
+import {createContext, useReducer, useState} from 'react'
+const MyContect = createContext();
+
+const reducer = (state, action)=>{
+    switch(action.type){
+        case 'insert' : return state 수정;
+    }
+}
+
+function Context({children}) {
+
+  const [data, dispatch] = useReducer(reducer, []);
+
+  return (
+    <MyContect.Provider value={ {data, dispatch} }>
+        {children}
+    </MyContect.Provider>
+  )
+}
+
+export {MyContect, Context}

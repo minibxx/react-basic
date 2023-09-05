7010ebec4d3d5b36f99092d80a549e1e807f2d54
minibxx <alsdbqls1004@gmail.com>
2023-09-05 16:28:27 +0900

    댓글 관리 실습 프론트

--- ex-front/src/App.js
+++ ex-front/src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+
+import {useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
 
+
+function List({data,setData}) {
+
+  const remove = (id)=>{
+    console.log(`${process.env.REACT_APP_SERVER}/abc/${id}`);
+
+    axios.delete(`${process.env.REACT_APP_SERVER}/abc/${id}`)
+    .then(res=>{
+        setData(res.data)
+    })
+  }
+
+  return(
+    <>
+      {
+        data.map(obj=>(
+          <li key={obj.id}>
+            {obj.msg}
+            <button className='del-bt' onClick={()=>{remove(obj.id)}}>삭제</button>
+          </li>
+        ))
+      }
+        
+    </>
+  )
+}
+
+function Write({setData}) {
+  const insert = (e)=>{
+    e.preventDefault();
+
+    let msg = e.target.msg.value;
+    axios.post(`${process.env.REACT_APP_SERVER}/insert`,{msg})
+    .then(res=>{
+      setData(res.data)
+    })
+  }
+
+
+  return(
+      <div>
+        <form onSubmit={insert}>
+          <input type="text" name="msg"/>
+          <input className='save-bt' type="submit" value="저장"/>
+        </form>
+      </div>
+  );
+}
+
 function App() {
+  const [data,setData]  = useState([]);
+
+  const getData = ()=>{
+    axios.get(`${process.env.REACT_APP_SERVER}/abc`)
+    .then(res=>{
+      setData(res.data);
+    });
+  }
+  
+  useEffect(() => {
+    getData();  
+  }, []);
+  
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article>
+      <div className='title'>한줄댓글(7)</div>
+      <Write setData={setData}/>
+
+      <ul>
+        <List data={data} setData={setData}/>
+      </ul>
+    </article>
   );
 }
 
